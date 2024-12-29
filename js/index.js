79ece5137818ab4f26cb8adef428c8d9be2200b1
niroulasus05 <susananiroula417@gmail.com>
2024-12-29 21:47:20 +0545

    index

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+console.log()
+
+
+
+// Operators
+
+// 1. assignment operators =, +=, -=, *=, /=, %=
+// 2. arithmetic operators +, -,*, /, %
+// 3. comparison operators ==, !=, >, <, >=, <=
+// 4. logical operators &&, ||, !
+// 5. ternary operator
+
+// Control Flow
+
+// If else
+
